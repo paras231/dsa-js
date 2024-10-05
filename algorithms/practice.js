2bac105fa1af5911f9a42129869ab985efbc897c
paras231 <sharmaparas553@gmail.com>
2024-10-05 11:55:14 +0530

    updated code for questions

--- algorithms/practice.js
+++ algorithms/practice.js
@@ -64,3 +64,71 @@ export function threeSum(arr) {
   }
   return result;
 }
+
+/**
+ * four sum problem leetcode
+ */
+
+export function fourSum(arr, target) {
+  let result = [];
+  // sort the array
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length - 3; i++) {
+    // skip duplicates for the first element
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
+
+    for (let j = i + 1; j < arr.length - 2; j++) {
+      // skip duplicates for the second element
+      if (j > i + 1 && arr[j] === arr[j - 1]) continue;
+
+      let left = j + 1;
+      let right = arr.length - 1;
+
+      while (left < right) {
+        let sum = arr[i] + arr[j] + arr[left] + arr[right];
+
+        if (sum === target) {
+          result.push([arr[i], arr[j], arr[left], arr[right]]);
+
+          // move the pointers to skip duplicates
+          while (left < right && arr[left] === arr[left + 1]) left++;
+          while (left < right && arr[right] === arr[right - 1]) right--;
+
+          left++;
+          right--;
+        } else if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+
+/**
+ * remove element
+ */
+
+
+export function removeElem(arr, val) {
+  let k = 0; // Index for the next position of a non-val element
+
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== val) {
+          arr[k] = arr[i];
+          k++;
+      }
+  }
+
+  // Return the new length
+  return k;
+}
+
+/**
+ * 
+ */
